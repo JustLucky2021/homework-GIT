81b885173973d495cb868707070f93958c7e76a5
JustLucky2021 <just_lucky2007@mail.ru>
2021-07-21 20:25:36 +0300

    make second activity

---
+++ app/src/main/java/com/example/homeworkgit/OrdersList.kt
@@ -0,0 +1,13 @@
+package com.example.homeworkgit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OrdersList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_orders_list)
+
+
+    }
+}
