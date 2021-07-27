24eb5325b3d8ac4d73c735626a133727914bdd59
JustLucky2021 <just_lucky2007@mail.ru>
2021-07-27 19:18:59 +0300

    made homework Intents

--- app/src/main/java/com/example/homeworkgit/MainActivity.kt
+++ app/src/main/java/com/example/homeworkgit/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.homeworkgit
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val btnStart = findViewById<Button>(R.id.btnStart)
+            btnStart.setOnClickListener {
+                val startOrdersList = Intent(this, OrdersList::class.java).apply {
+                    val phoneNumber = findViewById<EditText>(R.id.etPhoneNumber)
+                    putExtra("phoneNumber", "${phoneNumber.text}")
+                    startActivity(this)
+                }
+            }
+        }
     }
-}
--- app/src/main/java/com/example/homeworkgit/OrdersList.kt
+++ app/src/main/java/com/example/homeworkgit/OrdersList.kt
@@ -2,12 +2,16 @@ package com.example.homeworkgit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class OrdersList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_orders_list)
-
-
+        val phoneNum = intent.getStringExtra("phoneNumber")
+        Log.d("TAG", "Order: $phoneNum")
+        val recycle = findViewById<TextView>(R.id.tvRecycle)
+        recycle.append(" $phoneNum")
     }
 }
